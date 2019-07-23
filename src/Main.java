f1a5b685d99efccd3496216b0e85639f44c08822
ofirle <41347272+ofirle@users.noreply.github.com>
2019-07-23 20:30:54 +0300

    ofir 2030

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("aaaaaahhhhhh");
+        Path repositoryPathDirection;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Select:");
+        System.out.println("1. initialization");
+        System.out.println("\ninsert:1");
+        String s = in.nextLine();
+        if(s.equals("1"))
+        {
+            System.out.println("please insert the name of the repository:\n");
+            String repositoryName = in.nextLine();
+            System.out.println("please insert path of the repository:\n");
+            String repositoryPath = in.nextLine();
+            System.out.println("Repository Name: "+ repositoryName);
+            System.out.println("Repository Path: "+ repositoryPath);
+            repositoryPathDirection = Paths.get(repositoryPath);
+        }
+
     }
 }
