6a0f8ef37a7bf5f07bfb7bd617eee13098532530
amira <amiraloni7@gmail.com>
2019-07-31 21:29:53 +0300

    commit & 50% of Branch - 31/07/19 Amir & Ofir

--- src/Logic/Branch.java
+++ src/Logic/Branch.java
@@ -11,4 +11,12 @@ public class Branch {
     public String getName() {
         return name;
     }
+
+    public void setSH1_Commit(String SH1_Commit) {
+        this.SH1_Commit = SH1_Commit;
+    }
+
+    public String getSH1_Commit() {
+        return SH1_Commit;
+    }
 }
---
+++ src/Logic/BranchesManager.java
@@ -0,0 +1,33 @@
+package Logic;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
+import static Logic.ConstantsEnums.EmptyString;
+
+public class BranchesManager {
+    List<Branch> BranchArray;
+
+    public BranchesManager() {
+        BranchArray = new ArrayList<Branch>();
+    }
+
+    public String getSha1ofBranchByName(String i_BranchName)
+    {
+        String BranchSha1 =EmptyString;
+        for(Branch branch : BranchArray)
+        {
+            if(branch.getName().equals(i_BranchName))
+            {
+                BranchSha1 = branch.getSH1_Commit();
+            }
+        }
+        if(BranchSha1.equals(EmptyString)) {
+            //error: Branch Not Found
+        } else {
+
+            return BranchSha1;
+        }
+    }
+}
